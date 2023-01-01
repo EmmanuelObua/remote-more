8457a305469fce55a3068f3e9fe3726ddde8ec77
Emmanuel Obua <eobua6882@gmail.com>
2023-01-01 21:52:26 +0300

    implemeneted the index file with express and node js configs

---
+++ server/index.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let path = require('path');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let mongoDb = require('./database/db');
+
+mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', true);
+
+mongoose.connect(mongoDb.db, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(() => {
+	console.log('Database sucessfully connected ')
+},
+error => {
+	console.log('Database error: ' + error)
+})
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+require('./routes/store.routes')(app);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+	console.log('Listening on port ' + port)
+})
